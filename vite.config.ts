ee428ebb47911b2b611fad83f1131784f7190c81
Babaeva555 <tanja.perebeinos@yandex.ru>
2026-03-26 16:47:48 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,7 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  
+  // Добавьте это для GitHub Pages
+  base: '/Englishcoursefigma/', // Или '/название-вашего-репозитория/'
 })
